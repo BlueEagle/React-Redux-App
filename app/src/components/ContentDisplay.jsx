06119d9fe2694b9a540d0eca406023a9eb712363
Collin Ballou <BlueEagle@users.noreply.github.com>
2020-07-15 14:48:47 -0600

    wip

--- app/src/components/ContentDisplay.jsx
+++ app/src/components/ContentDisplay.jsx
@@ -1,14 +1,51 @@
 import React from 'react'
-import { Button } from '@material-ui/core'
+import { generateActivity } from '../store/actions'
+import { connect } from 'react-redux'
+import { Button, Paper, Box } from '@material-ui/core'
+import styled from 'styled-components'
+
 
 const ContentDisplay = props => {
+  // if (props.data.length !== 0) console.log('Data retrieved!')
+  console.log("Data value:", props.data)
 
   return (
     <div>
       <h3>Click below to generate fun activity ideas!</h3>
-      <Button variant="contained" color="primary">Generate activities!</Button>
+      <Button onClick={props.generateActivity} variant="contained" color="primary">Generate activities!</Button>
+      <StyledBox>
+        <Paper elevation={3}>
+          {props.isLoading && <p>Please wait, your data is loading...</p>}
+          {props.error && <p className="error">Something went wrong: {props.error}</p>}
+          {props.data !== null && (
+            <StyledDiv>
+              <h3>{props.data.activity}</h3>
+              {/* <h4>Price: {props.data.price}</h4> */}
+            </StyledDiv>
+          )}
+        </Paper>
+      </StyledBox>
     </div>
   )
 }
 
-export default ContentDisplay
+const mapStateToProps = state => {
+  return {
+    isLoading: state.isLoading,
+    data: state.data,
+    error: state.error
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  {generateActivity}
+)(ContentDisplay)
+
+const StyledDiv = styled.div`
+  padding: 1%;
+  word-wrap: wrap;
+`
+const StyledBox = styled(Box)`
+  margin-top: 2rem;
+`
--- app/src/store/reducers/index.js
+++ app/src/store/reducers/index.js
@@ -1,11 +1,31 @@
+import { FETCH_ACTIVITY_IDEA_START, FETCH_ACTIVITY_IDEA_SUCCESS, FETCH_ACTIVITY_IDEA_ERROR } from '../actions'
+
 const initialState = {
   isLoading: false,
-  data: [],
+  data: null,
   error: ''
 }
 
 export const reducer = (state = initialState, action) => {
   switch(action.type) {
+    case FETCH_ACTIVITY_IDEA_START:
+      return {
+        ...state,
+        isLoading: true
+      }
+    case FETCH_ACTIVITY_IDEA_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        data: action.payload,
+        error: ''
+      }
+    case FETCH_ACTIVITY_IDEA_ERROR:
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload
+      }
     default:
       return state
   }
